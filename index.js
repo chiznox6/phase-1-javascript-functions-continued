8da280a1bd2ec9bc958e7fd2e48b4f8699584103
James <james.kariuki5@student.moringaschool.com>
2025-06-15 12:04:52 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// Function 1: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function 2: mondayWork
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// Function 3: wrapAdjective
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+// Optional: Export functions if needed for testing (in Node.js)
+module.exports = {
+  saturdayFun,
+  mondayWork,
+  wrapAdjective
+};
